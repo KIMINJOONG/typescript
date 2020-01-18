84d04548ed5ab5b4d253cd173920984b33aa50c5
KIMINJOONG <ijboym216@gmail.com>
2020-01-18 15:25:11 +0900

    1.4 watch and gugudan

--- lecture/lecture.js
+++ lecture/lecture.js
@@ -1,3 +1,10 @@
-var a = 1;
-console.log("hello typescript");
-console.log(a);
+var numberOne = Math.ceil(Math.random() * 9);
+var numberTwo = Math.ceil(Math.random() * 9);
+var result = numberOne * numberTwo;
+var word = document.createElement("div");
+word.textContent = numberOne + " \uACF1\uD558\uAE30 " + numberTwo;
+document.body.append(word);
+var form = document.createElement("form");
+document.body.append(form);
+var input = document.createElement("input");
+input.type = "number";
